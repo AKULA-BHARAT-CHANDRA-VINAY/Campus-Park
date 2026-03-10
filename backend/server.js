8d8785075667a9f1c991c182abcd1eb9bc1b0177
AKULA-BHARAT-CHANDRA-VINAY <abcvinay2005@gmail.com>
2026-03-10 17:28:57 +0530

    fix railway port handling

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ import mlRoutes from "./routes/ml_Routes.js";
 import parkingRoutes from "./routes/parkingRoutes.js";
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 
 //middlewares
 const allowedOrigins = [
