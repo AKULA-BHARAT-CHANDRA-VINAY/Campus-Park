07b6270f51d7e91c8d187cf29a36fbd869c93d96
AKULA-BHARAT-CHANDRA-VINAY <abcvinay2005@gmail.com>
2026-03-26 13:14:37 +0530

    DB Corrections made

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "CampusPark"
+    });
     console.log("DB connected");
   } catch (error) {
     console.error("DB connection failed", error);
